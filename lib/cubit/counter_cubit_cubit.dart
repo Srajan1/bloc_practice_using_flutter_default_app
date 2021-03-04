05b170514ec1d103cd603a7ff54126ee05718abb
Srajan Kishor Gupta <srakishor.g@gmail.com>
2021-03-04 18:49:02 +0530

    counter using cubit

---
+++ lib/cubit/counter_cubit_cubit.dart
@@ -0,0 +1,12 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_cubit_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 0));
+
+  void increment() => emit(CounterState(counter: state.counter + 1));
+
+  void decrement() => emit(CounterState(counter: state.counter - 1));
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,75 @@
-import 'package:bloc_practice/Bloc/counter_bloc.dart';
+import 'package:bloc_practice/cubit/counter_cubit_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(MaterialApp(home: MyApp()));
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  final _counterBloc = CounterBloc();
+  @override
+  Widget build(BuildContext context) {
+    return new BlocProvider(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new MyHomePage(title: 'Flutter Hello World'),
+      ),
+    );
+  }
+}
 
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Demo Home Page'),
+      appBar: new AppBar(
+        title: new Text(widget.title),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text('The button has been pressed this many times')),
-          StreamBuilder(
-            initialData: 0,
-            stream: _counterBloc.counterStream,
-            builder: (context, snapshot) {
-              return Text(
-                snapshot.data.toString(),
-                style: Theme.of(context).textTheme.headline4,
-              );
-            },
-          ),
-        ],
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              _counterBloc.eventSink.add(counterActions.increment);
-            },
-            child: Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              _counterBloc.eventSink.add(counterActions.decrement);
-            },
-            child: Icon(Icons.remove),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              _counterBloc.eventSink.add(counterActions.reset);
-            },
-            child: Icon(Icons.loop),
-          )
-        ],
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'You have pushed the button this many times:',
+            ),
+            new BlocBuilder<CounterCubit, CounterState>(
+              builder: (context, state) {
+                return Text(
+                  state.counter.toString(),
+                  style: TextStyle(fontSize: 40),
+                );
+              },
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  child: Icon(Icons.add),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  child: Icon(Icons.remove),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
